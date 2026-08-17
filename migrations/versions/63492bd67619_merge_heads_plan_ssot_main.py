f76e8caaa9423b99b6dba210cb7d4dfb29c1750a
Abdelhamid Shalaby <abdelhamid@manasety.ai>
2026-08-18 01:59:21 +0300

    chore: merge migration heads (plan-ssot + main)

---
+++ migrations/versions/63492bd67619_merge_heads_plan_ssot_main.py
@@ -0,0 +1,24 @@
+"""merge heads: plan-ssot + main
+
+Revision ID: 63492bd67619
+Revises: k8l1m4n7p0q3, 5e85ab15047f
+Create Date: 2026-08-18 01:59:17.951391
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63492bd67619'
+down_revision = ('k8l1m4n7p0q3', '5e85ab15047f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
